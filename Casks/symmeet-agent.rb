93daad6d4c296c0338c193d8f64a2f5b18e83149
Daniel <justus.daniel@web.de>
2026-07-23 14:01:45 +0200

    symmeet v0.1.0

---
+++ Casks/symmeet-agent.rb
@@ -0,0 +1,18 @@
+cask "symmeet-agent" do
+  version "0.1.0"
+  sha256 "a5cd065a740e42332bfd1573ad366acbd71d5f64b382de99834d8c53115cc73e"
+
+  url "https://github.com/danieljustus/symaira-meet/releases/download/v#{version}/SymMeetAgent_v#{version}.dmg"
+  name "SymMeetAgent"
+  desc "Consent-first local meeting recording agent"
+  homepage "https://github.com/danieljustus/symaira-meet"
+
+  depends_on macos: :sequoia
+
+  app "SymMeetAgent.app"
+
+  zap trash: [
+    "~/Library/Caches/dev.symaira.symmeet.agent",
+    "~/Library/Preferences/dev.symaira.symmeet.agent.plist",
+  ]
+end
---
+++ Formula/symmeet.rb
@@ -0,0 +1,21 @@
+# typed: false
+# frozen_string_literal: true
+
+class Symmeet < Formula
+  desc "Local-first, standalone meeting-artifact tool"
+  homepage "https://github.com/danieljustus/symaira-meet"
+  url "https://github.com/danieljustus/symaira-meet/releases/download/v0.1.0/symmeet_v0.1.0_darwin_arm64.tar.gz"
+  version "0.1.0"
+  sha256 "a1aa86f5a5f4c2f288bf2909f68268e998b53b0be28c394398bdea02f20a59e3"
+  license "Apache-2.0"
+
+  depends_on macos: :sequoia
+
+  def install
+    bin.install "symmeet"
+  end
+
+  test do
+    system "#{bin}/symmeet", "version", "--json"
+  end
+end
